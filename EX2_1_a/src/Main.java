75642fbf8d8147dccd819f955c6d10ee1ec4511b
SarahMohamed312 <sarahmohamed12337@gmail.com>
2024-09-27 14:00:29 +0300

    Delete EX2_1_a directory

--- EX2_1_a/src/Main.java
+++
@@ -1,9 +0,0 @@
-/*Create a simple non-GUI Application that carries out the functionality of a basic
-calculator (addition, subtraction, multiplication, and division).*/
-
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    }}
